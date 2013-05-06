5f20aaa4b3586e6101b6e3fbfd642cb9eb384b4b
Charlerlin <charlerlin@gmail.com>
2013-05-06 16:50:11 +0200

    Premier commit avec la base du modèle et les fichiers : classpath project

---
+++ bdtheque/src/model/Album.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Album {
+	private String titre, auteur, serie, genre, synopsis, commentaire;
+	private int noserie, note, nbplanches, prix;
+
+}
---
+++ bdtheque/test/model/TestAlbum.java
@@ -0,0 +1,10 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAlbum {
+
+}
