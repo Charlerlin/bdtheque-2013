6bdbdcfd0d951942f19e2ec11d99e6f8f1c2b7d8
Aegils <edouard.vyncke@gmail.com>
2013-05-07 11:03:13 +0200

    Ajout constructeur album(Test commit)

--- bdtheque/src/model/Album.java
+++ bdtheque/src/model/Album.java
@@ -4,4 +4,8 @@ public class Album {
 	private String titre, auteur, serie, genre, synopsis, commentaire;
 	private int noserie, note, nbplanches, prix;
 
+	
+	public Album() {
+		// TODO Auto-generated constructor stub
+	}
 }
