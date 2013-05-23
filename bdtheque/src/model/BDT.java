3f0d74227b55451ef80f696f39c3ca50dba22c6d
Charlerlin <charlerlin@gmail.com>
2013-05-23 19:26:45 +0200

    Ajout de la classe TestBDT (vide) Ajout de la série dans la gestion d'index de BDT

--- bdtheque/src/model/BDT.java
+++ bdtheque/src/model/BDT.java
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 
 public class BDT {
 	private HashMap<Integer, Album> listeAlbums;
-	private TreeMap<String, Integer> listeTitres, listeGenres, listeAuteurs, listeSeries;
+	private TreeMap<String, Integer> listeTitres, listeSeries, listeAuteurs, listeGenres;
 
 	public BDT(){
 		listeAlbums = new HashMap<Integer, Album>();
@@ -27,6 +27,7 @@ public class BDT {
 			listeAlbums.put(a.hashCode(), a);
 			gestionIndexAjout(a, "titre");
 			gestionIndexAjout(a, "auteurs");
+			gestionIndexAjout(a, "serie");
 			gestionIndexAjout(a, "genre");
 			return true;
 		}
@@ -35,22 +36,40 @@ public class BDT {
 	private void gestionIndexAjout(Album a, String idx){
 		if(idx.equals("titre")){
 			if(listeTitres.containsKey(a.getTitre())){
-				int nbTitres = listeTitres.remove(a.getTitre());
+				int nbTitres = listeTitres.remove(a.getTitre()); //nombre d'albums ayant ce titre
 				listeTitres.put(a.getTitre(), ++nbTitres);
 			}
 			else{
 				listeTitres.put(a.getTitre(), 1);
 			}
 		}
+		if(idx.equals("serie")){
+			if(listeSeries.containsKey(a.getSerie())){
+				int nbSerie = listeSeries.remove(a.getSerie()); //nombre d'albums dans la série
+				listeSeries.put(a.getSerie(), ++nbSerie);
+			}
+			else{
+				listeSeries.put(a.getSerie(), 1);
+			}
+		}
 		if(idx.equals("auteurs")){
 			for(String aut : a.getAuteurs()){
 				if(listeAuteurs.containsKey(aut)){
-					int nbAuteurs = listeAuteurs.remove(aut);
+					int nbAuteurs = listeAuteurs.remove(aut); //nombre d'albums correspondants à cet auteur
 					listeAuteurs.put(aut, ++nbAuteurs);
 				}
 				else{
 					listeAuteurs.put(aut, 1);
 				}
+			}
+		}
+		if(idx.equals("genre")){
+			if(listeGenres.containsKey(a.getTitre())){
+				int nbGenres = listeGenres.remove(a.getGenre()); //nombre d'albums de ce genre
+				listeGenres.put(a.getGenre(), ++nbGenres);
+			}
+			else{
+				listeGenres.put(a.getGenre(), 1);
 			}
 		}
 	}
